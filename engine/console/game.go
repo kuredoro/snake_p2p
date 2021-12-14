d5362ef614b61aa87cf4af8612ad9ad0ab2b799d
alphy1 <42415340+alphy1@users.noreply.github.com>
2021-12-14 23:11:50 +0300

    console: refactor drawing functions from `main` to `game`.

--- engine/console/game.go
+++ engine/console/game.go
@@ -1,8 +1,10 @@
 package console
 
 import (
+	"fmt"
 	"github.com/gdamore/tcell/v2"
 	"github.com/kuredoro/snake_p2p/core"
+	"math/rand"
 )
 
 type Snake struct {
@@ -15,4 +17,113 @@ type Snake struct {
 type Game struct {
 	snakes []Snake
 	food []core.Coord
+}
+
+type Boundary struct {
+	topLeft     core.Coord
+	bottomRight core.Coord
+}
+
+func (boundary Boundary) Contains(coord core.Coord) bool {
+	return (coord.X <= boundary.topLeft.X || coord.X >= boundary.bottomRight.X) ||
+		(coord.Y <= boundary.topLeft.Y || coord.Y >= boundary.bottomRight.Y)
+}
+
+func drawText(s tcell.Screen, x1, y1, x2, y2 int, style tcell.Style, text string) {
+	row := y1
+	col := x1
+	for _, r := range []rune(text) {
+		s.SetContent(col, row, r, nil, style)
+		col++
+		if col >= x2 {
+			row++
+			col = x1
+		}
+		if row > y2 {
+			break
+		}
+	}
+}
+
+func drawInitialBox(s tcell.Screen, boundary Boundary, style tcell.Style) {
+	x1, y1 := boundary.topLeft.X, boundary.topLeft.Y
+	x2, y2 := boundary.bottomRight.X, boundary.bottomRight.Y
+	if y2 < y1 {
+		y1, y2 = y2, y1
+	}
+	if x2 < x1 {
+		x1, x2 = x2, x1
+	}
+
+	// Fill background
+	for row := y1; row <= y2; row++ {
+		for col := x1; col <= x2; col++ {
+			s.SetContent(col, row, tcell.RuneBullet, nil, style)
+		}
+	}
+
+	// Draw borders
+	for col := x1; col <= x2; col++ {
+		s.SetContent(col, y1, tcell.RuneHLine, nil, style)
+		s.SetContent(col, y2, tcell.RuneHLine, nil, style)
+	}
+	for row := y1 + 1; row < y2; row++ {
+		s.SetContent(x1, row, tcell.RuneVLine, nil, style)
+		s.SetContent(x2, row, tcell.RuneVLine, nil, style)
+	}
+
+	// Only draw corners if necessary
+	if y1 != y2 && x1 != x2 {
+		s.SetContent(x1, y1, tcell.RuneULCorner, nil, style)
+		s.SetContent(x2, y1, tcell.RuneURCorner, nil, style)
+		s.SetContent(x1, y2, tcell.RuneLLCorner, nil, style)
+		s.SetContent(x2, y2, tcell.RuneLRCorner, nil, style)
+	}
+
+	//drawText(s, x1+1, y1+1, x2-1, y2-1, style, text)
+}
+
+func drawSnake(s tcell.Screen, snake Snake, boundary Boundary) error {
+	if boundary.Contains(snake.head) {
+		return fmt.Errorf("snakep2p's head coordinates are out of boundary")
+	}
+	s.SetContent(snake.head.X, snake.head.Y, tcell.RuneBullet, nil, snake.style)
+	for _, point := range snake.body {
+		if boundary.Contains(point) {
+			return fmt.Errorf("snakep2p's body coordinates are out of boundary")
+		}
+		s.SetContent(point.X, point.Y, tcell.RuneBlock, nil, snake.style)
+	}
+	return nil
+}
+
+func drawFood(s tcell.Screen, food core.Coord, style tcell.Style, boundary Boundary) error {
+	if boundary.Contains(food) {
+		return fmt.Errorf("food coordinates are out of boundary")
+	}
+	s.SetContent(food.X, food.Y, '*', nil, style)
+	return nil
+}
+
+func drawGridCell(s tcell.Screen, cell core.Coord, style tcell.Style, boundary Boundary) error {
+	if boundary.Contains(cell) {
+		return fmt.Errorf("cell is out of boundary")
+	}
+	s.SetContent(cell.X, cell.Y, tcell.RuneBullet, nil, style)
+	return nil
+}
+
+func getRandColor(defColors map[tcell.Color]struct{}) tcell.Color {
+	color := tcell.PaletteColor(rand.Intn(256))
+	_, ok := defColors[color]
+	for ok == true {
+		color = tcell.PaletteColor(rand.Intn(256))
+		_, ok = defColors[color]
+	}
+	return color
+}
+
+func genSnakeStyle(defColors map[tcell.Color]struct{}) tcell.Style {
+	style := tcell.StyleDefault.Foreground(getRandColor(defColors)).Background(getRandColor(defColors))
+	return style
 }
