b06a0b642a63b93c1f7701c9b37f9272e450700f
alphy1 <42415340+alphy1@users.noreply.github.com>
2021-12-14 23:07:57 +0300

    console: created `Game` struct without any functionalities.

--- engine/console/game.go
+++ engine/console/game.go
@@ -1,3 +1,18 @@
 package console
 
+import (
+	"github.com/gdamore/tcell/v2"
+	"github.com/kuredoro/snake_p2p/core"
+)
 
+type Snake struct {
+	id    int         // snakep2p id
+	body  []core.Coord     // coordinates of snakep2p's body
+	head  core.Coord       // coordinates of snakep2p's head
+	style tcell.Style // snakep2p's style
+}
+
+type Game struct {
+	snakes []Snake
+	food []core.Coord
+}
