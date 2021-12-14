ce5e8c9b71aac46b384b93892ec94fea254adba8
alphy1 <42415340+alphy1@users.noreply.github.com>
2021-12-14 21:29:01 +0300

    core: added basic game events.

---
+++ core/game_events.go
@@ -0,0 +1,41 @@
+package core
+
+type Coord struct {
+	X, Y int
+}
+
+type PlayerStarts struct {
+	Players map[int]Coord  // map from player's ID to its start coordinates
+}
+
+type NewFood struct {
+	Pos Coord    // coordinates of new food
+}
+
+type Direction int
+
+const (
+	UP Direction = iota
+	Right
+	Down
+	Left
+)
+type PlayerMove struct {
+	Moves map[int] Direction   // map from player's ID to direction of it's move
+}
+
+type PlayerDied struct {
+	ID int     // ID of player who died
+}
+
+type FoodEaten struct {
+	ID int     // ID of player who ate food
+	Pos Coord  // coordinates of eaten food
+}
+
+type Tick struct{}
+
+type GameOver struct {
+	Successful bool    // did game finish without errors or not
+	Winner int      // ID of winner player
+}
