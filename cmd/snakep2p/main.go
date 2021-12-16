39eb1a219dc8a6497eb7bb45ac7080319b049c75
alphy1 <42415340+alphy1@users.noreply.github.com>
2021-12-16 18:27:37 +0300

    snakep2p: change the order of the events (`Tick` after `PlayerMove`).

--- cmd/snakep2p/main.go
+++ cmd/snakep2p/main.go
@@ -12,41 +12,46 @@ func main() {
 	// Create Game
 	game := console.GameInit(make(chan interface{}, 100))
 	// Add events to channel
-	game.Ch <- core.PlayerStarts{Players: map[int]core.Coord{0: {X: 4, Y: 2},
-															 1: {X: 7, Y: 5},
-															 2: {X: 10, Y: 2},
-															 3: {X: 10, Y: 6}}}
+	game.Ch <- core.PlayerStarts{Players: map[int]core.Coord{
+		0: {X: 4, Y: 2},
+		1: {X: 7, Y: 5},
+		2: {X: 10, Y: 2},
+		3: {X: 10, Y: 6}}}
 	game.Ch <- core.Tick{}
 	game.Ch <- core.NewFood{FoodID: 0, Pos: core.Coord{X: 5, Y: 2}}
-	game.Ch <- core.Tick{}
 	game.Ch <- core.FoodEaten{FoodID: 0}
-	game.Ch <- core.PlayerMove{Moves: map[int]core.Direction{0: core.Right,
-															 1: core.Left,
-															 2: core.Down,
-															 3: core.Up}}
 	game.Ch <- core.PushSegment{SnakeID: 0, Pos: core.Coord{X: 4, Y: 2}}
+	game.Ch <- core.PlayerMove{Moves: map[int]core.Direction{
+		0: core.Right,
+		1: core.Left,
+		2: core.Down,
+		3: core.Up}}
+	game.Ch <- core.Tick{}
 	game.Ch <- core.NewFood{FoodID: 1, Pos: core.Coord{X: 5, Y: 3}}
-	game.Ch <- core.Tick{}
 	game.Ch <- core.NewFood{FoodID: 2, Pos: core.Coord{X: 10, Y: 4}}
-	game.Ch <- core.Tick{}
-	game.Ch <- core.PlayerMove{Moves: map[int]core.Direction{0: core.Down,
-															 1: core.Up,
-															 2: core.Down,
-															 3: core.Right}}
 	game.Ch <- core.FoodEaten{FoodID: 1}
 	game.Ch <- core.FoodEaten{FoodID: 2}
 	game.Ch <- core.PushSegment{SnakeID: 0, Pos: core.Coord{X: 4, Y: 2}}
 	game.Ch <- core.PushSegment{SnakeID: 2, Pos: core.Coord{X: 10, Y: 3}}
+	game.Ch <- core.PlayerMove{Moves: map[int]core.Direction{
+		0: core.Down,
+		1: core.Up,
+		2: core.Down,
+		3: core.Right}}
+	game.Ch <- core.Tick{}
 	game.Ch <- core.PlayerMove{Moves: map[int]core.Direction{0: core.Down,
 															 1: core.Right,
 															 2: core.Down,
 															 3: core.Left}}
+	game.Ch <- core.Tick{}
 	game.Ch <- core.PlayerDied{SnakeID: 2}
 	game.Ch <- core.PlayerDied{SnakeID: 3}
 	game.Ch <- core.PlayerMove{Moves: map[int]core.Direction{0: core.Down,
 															 1: core.Left}}
+	game.Ch <- core.Tick{}
 	game.Ch <- core.PlayerMove{Moves: map[int]core.Direction{0: core.Down,
 															 1: core.Left}}
+	game.Ch <- core.Tick{}
 	game.Ch <- core.PlayerDied{SnakeID: 1}
 	game.Ch <- core.GameOver{Successful: true, Winner: 0}
 	game.RunGame()
