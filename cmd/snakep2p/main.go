e02378af26c3105a68465cd14157cce5243d9cfe
しずえ <66309775+kuredoro@users.noreply.github.com>
2021-12-17 20:18:34 +0300

    snakep2p: fixed fake game events' order.

--- cmd/snakep2p/main.go
+++ cmd/snakep2p/main.go
@@ -16,34 +16,31 @@ func eventsSequence(game *console.Game) {
 		2: {X: 10, Y: 2},
 		3: {X: 10, Y: 6},
 	}}
+	game.Ch <- core.NewFood{FoodID: 0, Pos: core.Coord{X: 5, Y: 2}}
 	game.Ch <- core.Tick{}
 	time.Sleep(2 * time.Second)
-	game.Ch <- core.NewFood{FoodID: 0, Pos: core.Coord{X: 5, Y: 2}}
-	time.Sleep(2 * time.Second)
-	game.Ch <- core.FoodEaten{FoodID: 0}
-	game.Ch <- core.PushSegment{SnakeID: 0, Pos: core.Coord{X: 4, Y: 2}}
 	game.Ch <- core.PlayerMove{Moves: map[int]core.Direction{
 		0: core.Right,
 		1: core.Left,
 		2: core.Down,
 		3: core.Up,
 	}}
+	game.Ch <- core.FoodEaten{FoodID: 0}
+	game.Ch <- core.PushSegment{SnakeID: 0, Pos: core.Coord{X: 4, Y: 2}}
+	game.Ch <- core.NewFood{FoodID: 1, Pos: core.Coord{X: 5, Y: 3}}
+	game.Ch <- core.NewFood{FoodID: 2, Pos: core.Coord{X: 10, Y: 4}}
 	game.Ch <- core.Tick{}
 	time.Sleep(2 * time.Second)
-	game.Ch <- core.NewFood{FoodID: 1, Pos: core.Coord{X: 5, Y: 3}}
-	time.Sleep(2 * time.Second)
-	game.Ch <- core.NewFood{FoodID: 2, Pos: core.Coord{X: 10, Y: 4}}
-	time.Sleep(2 * time.Second)
-	game.Ch <- core.FoodEaten{FoodID: 1}
-	game.Ch <- core.FoodEaten{FoodID: 2}
-	game.Ch <- core.PushSegment{SnakeID: 0, Pos: core.Coord{X: 4, Y: 2}}
-	game.Ch <- core.PushSegment{SnakeID: 2, Pos: core.Coord{X: 10, Y: 3}}
 	game.Ch <- core.PlayerMove{Moves: map[int]core.Direction{
 		0: core.Down,
 		1: core.Up,
 		2: core.Down,
 		3: core.Right,
 	}}
+	game.Ch <- core.FoodEaten{FoodID: 1}
+	game.Ch <- core.FoodEaten{FoodID: 2}
+	game.Ch <- core.PushSegment{SnakeID: 0, Pos: core.Coord{X: 4, Y: 2}}
+	game.Ch <- core.PushSegment{SnakeID: 2, Pos: core.Coord{X: 10, Y: 3}}
 	game.Ch <- core.Tick{}
 	time.Sleep(2 * time.Second)
 	game.Ch <- core.PlayerMove{Moves: map[int]core.Direction{
@@ -52,14 +49,8 @@ func eventsSequence(game *console.Game) {
 		2: core.Down,
 		3: core.Left,
 	}}
-	game.Ch <- core.Tick{}
-	time.Sleep(2 * time.Second)
 	game.Ch <- core.PlayerDied{SnakeID: 2}
 	game.Ch <- core.PlayerDied{SnakeID: 3}
-	game.Ch <- core.PlayerMove{Moves: map[int]core.Direction{
-		0: core.Down,
-		1: core.Left,
-	}}
 	game.Ch <- core.Tick{}
 	time.Sleep(2 * time.Second)
 	game.Ch <- core.PlayerMove{Moves: map[int]core.Direction{
@@ -68,7 +59,13 @@ func eventsSequence(game *console.Game) {
 	}}
 	game.Ch <- core.Tick{}
 	time.Sleep(2 * time.Second)
+	game.Ch <- core.PlayerMove{Moves: map[int]core.Direction{
+		0: core.Down,
+		1: core.Left,
+	}}
 	game.Ch <- core.PlayerDied{SnakeID: 1}
+	game.Ch <- core.Tick{}
+	time.Sleep(2 * time.Second)
 	game.Ch <- core.GameOver{Successful: true, Winner: 0}
 }
 
