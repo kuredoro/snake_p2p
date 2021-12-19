30fff60944b376bd78cebe2bcc568b58a7d522f0
alphy1 <42415340+alphy1@users.noreply.github.com>
2021-12-19 16:01:03 +0300

    console: create initial user view.

--- engine/console/gui.go
+++ engine/console/gui.go
@@ -1,37 +1,99 @@
 package console
 
 import (
+	"fmt"
 	"github.com/gdamore/tcell/v2"
 	"github.com/rivo/tview"
+	"strconv"
 )
 
-const (
-	welcome    = `Welcome to the multiplayer snake game.`
-	navigation = `Choose one of the following actions`
-)
+type GatherUI struct {
+	app *tview.Application
+	flex *tview.Flex
+	myGatherPoint *tview.TextView
+	gameList *tview.Table
+	createBtn *tview.Button
+	newGame *tview.InputField
+	maxPlayers int
+	joinGames int
+}
 
-func Cover() (content tview.Primitive) {
-	// Create a frame for the subtitle and navigation infos.
-	frame := tview.NewFrame(tview.NewBox()).
-		SetBorders(0, 0, 0, 0, 0, 0).
-		AddText(welcome, true, tview.AlignCenter, tcell.ColorGreen).
-		AddText("", true, tview.AlignCenter, tcell.ColorWhite).
-		AddText(navigation, true, tview.AlignCenter, tcell.ColorDarkMagenta)
+func checkNewGameField(textToCheck string, lastChar rune) bool {
+	_, err := strconv.Atoi(textToCheck)
+	if err != nil {
+		return false
+	}
+	return true
+}
 
-	createGamebtn := tview.NewButton("Create game").SetSelectedFunc(func() {})
-	joinGamebtn := tview.NewButton("Join game").SetSelectedFunc(func() {})
+func GatherUIInit() *GatherUI {
+	g := &GatherUI{}
+	g.app = tview.NewApplication()
+	g.myGatherPoint = tview.NewTextView().
+						SetRegions(true).
+						SetDynamicColors(true).
+						SetWordWrap(true).
+						SetChangedFunc(func() { g.app.Draw() })
+	g.myGatherPoint.SetBorder(true).SetTitle("My Gather Point")
+	fmt.Fprintf(g.myGatherPoint, "No gather point created.")
 
-	// Create a Flex layout that centers the logo and subtitle.
-	flex := tview.NewFlex().
+	table := tview.NewTable().
+		SetFixed(1, 1).
+		SetSelectable(true, false)
+	tableCell := tview.NewTableCell("ID").
+		SetTextColor(tcell.ColorYellow).
+		SetAlign(tview.AlignCenter).
+		SetExpansion(1)
+	table.SetCell(1, 1, tableCell)
+	tableCell = tview.NewTableCell("Players needed").
+		SetTextColor(tcell.ColorYellow).
+		SetAlign(tview.AlignCenter).
+		SetExpansion(1)
+	table.SetCell(1, 2, tableCell)
+	tableCell = tview.NewTableCell("Signed").
+		SetTextColor(tcell.ColorYellow).
+		SetAlign(tview.AlignCenter).
+		SetExpansion(1)
+	table.SetCell(1, 3, tableCell)
+	g.gameList = table
+
+	g.newGame = tview.NewInputField().
+					SetLabel("Enter the maximum number of players ").
+					SetFieldWidth(0).
+					SetFieldBackgroundColor(tcell.ColorBlack)
+
+	g.newGame.SetDoneFunc(func(key tcell.Key) {
+		if key != tcell.KeyEnter {
+			// we don't want to do anything if they just tabbed away
+			return
+		}
+		line := g.newGame.GetText()
+		if _, err := strconv.Atoi(line); err != nil {
+			//print("It is not number", err)
+			// ignore not numbers
+			return
+		}
+		g.maxPlayers, _ = strconv.Atoi(line)
+		g.myGatherPoint.Clear()
+		fmt.Fprintf(g.myGatherPoint, "Max # of players: %d", g.maxPlayers)
+		g.flex.RemoveItem(g.newGame)
+	})
+
+	g.createBtn = tview.NewButton("Create Game").SetSelectedFunc(func() {
+		g.flex.RemoveItem(g.createBtn)
+		g.flex.AddItem(g.newGame, 0, 1, false)
+	})
+
+	g.flex = tview.NewFlex().
 		SetDirection(tview.FlexRow).
-		AddItem(tview.NewBox(), 0, 5, false).
-		AddItem(frame, 0, 1, false).
-		AddItem(tview.NewFlex().
-			SetDirection(tview.FlexColumn).
-			AddItem(tview.NewBox(), 0, 1, false).
-			AddItem(createGamebtn, 20, 1, true).
-			AddItem(joinGamebtn, 20, 1, true).
-			AddItem(tview.NewBox(), 0, 1, false), 1, 1, true).
-		AddItem(tview.NewBox(), 0, 5, false)
-	return flex
+		AddItem(g.myGatherPoint, 3, 1, false).
+		AddItem(g.gameList, 0, 3, false).
+		AddItem(g.createBtn, 2, 1, false)
+
+	g.app.SetRoot(g.flex, true).EnableMouse(true)
+	return g
+}
+
+func (gatherUI *GatherUI)Run() error {
+	return gatherUI.app.Run()
 }
