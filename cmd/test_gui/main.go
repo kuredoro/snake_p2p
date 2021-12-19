f53fddacf84c709b26d5976aac363fe67fd34581
alphy1 <42415340+alphy1@users.noreply.github.com>
2021-12-19 17:33:34 +0300

    tets_gui: create node and pass it to `GatherUI`.

--- cmd/test_gui/main.go
+++ cmd/test_gui/main.go
@@ -1,13 +1,33 @@
 package main
 
 import (
+	"flag"
+	snake "github.com/kuredoro/snake_p2p"
 	"github.com/kuredoro/snake_p2p/engine/console"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+	"golang.org/x/net/context"
+	"os"
 
 	//"github.com/rivo/tview"
 )
 
 func main() {
-	gameUI := console.NewGatherUI()
+	//gatherFlag := flag.Int("gather", 0, "create gather point for N players")
+	flag.Parse()
+
+	f, _ := os.Create("ui_logs")
+	log.Logger = log.Output(zerolog.ConsoleWriter{Out: f})
+	ctx := context.Background()
+	h, err := snake.New(ctx)
+	if err != nil {
+		log.Err(err).Msg("New node")
+		os.Exit(1)
+	}
+
+	log.Info().Msg("Node initialized")
+
+	g := console.NewGatherUI(h)
 	// Shortcuts to navigate the slides.
 	//console.App.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
 	//	if event.Key() == tcell.KeyCtrlN {
@@ -24,8 +44,7 @@ func main() {
 	//})
 
 	// Start the application.
-	err := gameUI.Run()
-	if err != nil {
+	if err := g.Run(); err != nil {
 		panic("Game Run finished with error")
 	}
 }
