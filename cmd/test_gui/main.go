371a0103e87e9ac4d8dcd991e33273592974fa64
alphy1 <42415340+alphy1@users.noreply.github.com>
2021-12-19 10:50:19 +0300

    cmd: create main function to test gui.

---
+++ cmd/test_gui/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"github.com/kuredoro/snake_p2p/engine/console"
+	"github.com/gdamore/tcell/v2"
+	"github.com/rivo/tview"
+)
+
+// The application.
+var app = tview.NewApplication()
+
+func main() {
+	// Shortcuts to navigate the slides.
+	app.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
+		if event.Key() == tcell.KeyCtrlN {
+			//nextSlide()
+			return nil
+		} else if event.Key() == tcell.KeyCtrlP {
+			//previousSlide()
+			return nil
+		}
+		return event
+	})
+
+	// Start the application.
+	if err := app.SetRoot(console.Cover(), true).EnableMouse(true).Run(); err != nil {
+		panic(err)
+	}
+}
