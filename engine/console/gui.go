45f2047a709194b9994042316e72bc625034e6cb
alphy1 <42415340+alphy1@users.noreply.github.com>
2021-12-19 10:54:23 +0300

    console: created welcome page with two possible actions (join and create game).

--- engine/console/gui.go
+++ engine/console/gui.go
@@ -1,12 +1,37 @@
 package console
 
 import (
+	"github.com/gdamore/tcell/v2"
 	"github.com/rivo/tview"
 )
 
-func TviewTest() {
-	box := tview.NewBox().SetBorder(true).SetTitle("Hello, let's play snake game :)")
-	if err := tview.NewApplication().SetRoot(box, true).Run(); err != nil {
-		panic(err)
-	}
+const (
+	welcome    = `Welcome to the multiplayer snake game.`
+	navigation = `Choose one of the following actions`
+)
+
+func Cover() (content tview.Primitive) {
+	// Create a frame for the subtitle and navigation infos.
+	frame := tview.NewFrame(tview.NewBox()).
+		SetBorders(0, 0, 0, 0, 0, 0).
+		AddText(welcome, true, tview.AlignCenter, tcell.ColorGreen).
+		AddText("", true, tview.AlignCenter, tcell.ColorWhite).
+		AddText(navigation, true, tview.AlignCenter, tcell.ColorDarkMagenta)
+
+	createGamebtn := tview.NewButton("Create game").SetSelectedFunc(func() {})
+	joinGamebtn := tview.NewButton("Join game").SetSelectedFunc(func() {})
+
+	// Create a Flex layout that centers the logo and subtitle.
+	flex := tview.NewFlex().
+		SetDirection(tview.FlexRow).
+		AddItem(tview.NewBox(), 0, 5, false).
+		AddItem(frame, 0, 1, false).
+		AddItem(tview.NewFlex().
+			SetDirection(tview.FlexColumn).
+			AddItem(tview.NewBox(), 0, 1, false).
+			AddItem(createGamebtn, 20, 1, true).
+			AddItem(joinGamebtn, 20, 1, true).
+			AddItem(tview.NewBox(), 0, 1, false), 1, 1, true).
+		AddItem(tview.NewBox(), 0, 5, false)
+	return flex
 }
